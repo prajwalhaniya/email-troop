54e264b57f754b3d6c3a6fbfe20a4d4b18415d11
Prajwal <prajwal.hn@vedak.com>
2023-10-08 22:59:48 +0530

    add model for email

---
+++ backend/models/email.js
@@ -0,0 +1,57 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, Datatypes) => {
+    class email extends Model {
+        // static associate(models) {
+        // }
+    }
+    email.init(
+        {
+            id: {
+                type: Datatypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            mailId: {
+                type: Datatypes.INTEGER,
+                unique: true,
+                allowNull: false,
+            },
+            from: {
+                type: Datatypes.STRING(500),
+                allowNull: true,
+                defaultValue: null,
+            },
+            to: {
+                type: Datatypes.STRING(1000),
+                allowNull: true,
+                defaultValue: null,
+            },
+            message: {
+                type: Datatypes.TEXT,
+                allowNull: true,
+                defaultValue: null,
+            },
+            subject: {
+                type: Datatypes.TEXT,
+                allowNull: true,
+                defaultValue: null,
+            },
+            cc: {
+                type: Datatypes.STRING(1000),
+                allowNull: true,
+                defaultValue: null,
+            },
+            date: {
+                type: Datatypes.DATE,
+                allowNull: true,
+                defaultValue: null,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'email',
+        }
+    );
+    return email;
+};
